6aa9acf997a07d013e7729efa7c58125786db6b5
artem <artem.the.freelancer@gmail.com>
2022-04-07 13:55:25 +0400

    Added React-based palette

--- pixel_cell.js
+++ pixel_cell.js
@@ -1,47 +1,85 @@
 'use strict';
 
 const e = React.createElement;
+var globalColor = '#000000';
 
-var randomColor = function() {
-    let colors = [
-        '#C922D5',
-        '#D2691E',
-        '#E9967A',
-        '#2E8B57',
-        '#E0FFFF',
-        '#5F9EA0',
-        '#4169E1',
-        '#F5F5DC',
-        '#708090',
-        '#FFFF00',
-    ]
-    return colors[Math.floor(Math.random() * colors.length)];
+var getCurrentColor = function() {
+    return globalColor;
 }
 
-class PixelCell extends React.Component {
+var setCurrentColor = function( color ) {
+    globalColor = color;
+}
+
+
+class Palette extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { color: false };
   }
 
   render() {
-    if (this.state.color) {
-      console.log(this.state.color);
-      return e(
-        'div',
-        { className: "px-canvas-cell", style: { 'backgroundColor': this.state.color}, onClick: () => this.setState({ color: randomColor() }) },
-        ''
-      );
-    }
+    const listCells = this.props.availableColors.map((color) =>
+      <li key={color.toString()}>
+        <PaletteCell color={color} selected={false} />
+      </li>);
 
-    return e(
-      'div',
-      { className: "px-canvas-cell", onClick: () => this.setState({ color: randomColor() }) },
-      ''
-    );
+    return <ul>{listCells}</ul>;
+  }
+}
+
+class PaletteCell extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { selected: props.selected } ;
+  }
+
+  render() {
+    
+    const cellStyle = { 
+      backgroundColor: this.props.color
+    };
+
+    if (this.state.selected) {
+      console.log(getCurrentColor());
+      
+
+      return <div 
+        className="px-palette-cell cell-selected" 
+        style={cellStyle} 
+        onClick={() => this.setState({ selected: ! this.state.selected })}></div>
+    }
+    else {
+        return <div 
+        className="px-palette-cell cell-not-selected" 
+        style={cellStyle} 
+        onClick={() => {
+          setCurrentColor(this.props.color);
+          this.setState({ selected: ! this.state.selected });
+        }}></div>
+    }
   }
 }
 
 
-const domContainer = document.querySelector('#pixel_canvas');
-ReactDOM.render(e(PixelCell), domContainer);
+const domContainer = document.querySelector('#palette');
+ReactDOM.render(<Palette availableColors={["#00FF00", "#0000FF", "#FF0000"]}/>, domContainer);
+
+
+var canvas = document.getElementById('canvas');
+    
+
+if (canvas.getContext) {
+  var ctx = canvas.getContext('2d');
+
+  canvas.addEventListener("click", drawPixel);
+}
+
+var pixelWidth = 10;
+var pixelHeight = 10;
+
+function drawPixel(event) {
+  let pixelX = Math.floor(event.offsetX / pixelWidth) * pixelWidth;
+  let pixelY = Math.floor(event.offsetY / pixelHeight) * pixelHeight;
+  ctx.fillStyle = getCurrentColor();
+  ctx.fillRect(pixelX, pixelY, pixelWidth, pixelHeight);
+}

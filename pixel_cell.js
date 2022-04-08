f0a16ca2d0aa57a4fd606d71cf0d89d20553b877
artem <artem.the.freelancer@gmail.com>
2022-04-08 14:29:22 +0400

    Fixed selecting current color in the palette

--- pixel_cell.js
+++ pixel_cell.js
@@ -15,12 +15,22 @@ var setCurrentColor = function( color ) {
 class Palette extends React.Component {
   constructor(props) {
     super(props);
+    this.changeSelectedColor = this.changeSelectedColor.bind(this);
+    this.state = { selectedColor: props.selectedColor };
+    setCurrentColor( props.selectedColor );
+  }
+
+  changeSelectedColor( newColor ) {
+    console.log('New color:' + newColor);
+    this.setState({selectedColor: newColor});
+    setCurrentColor(newColor);
   }
 
   render() {
+    console.log('render palette with this.state.selectedColor = ' + this.state.selectedColor);
     const listCells = this.props.availableColors.map((color) =>
       <li key={color.toString()}>
-        <PaletteCell color={color} selected={false} />
+        <PaletteCell color={color} selected={ this.state.selectedColor == color } actionWhenClicked={this.changeSelectedColor}/>
       </li>);
 
     return <ul>{listCells}</ul>;
@@ -30,39 +40,36 @@ class Palette extends React.Component {
 class PaletteCell extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { selected: props.selected } ;
+    this.clicked = this.clicked.bind(this);
+  }
+
+  clicked() {
+    this.props.actionWhenClicked(this.props.color);
   }
 
   render() {
-    
-    const cellStyle = { 
-      backgroundColor: this.props.color
-    };
+    const cellStyle = { backgroundColor: this.props.color };
+    const cellClasses = this.props.selected ? "px-palette-cell cell-selected" : "px-palette-cell cell-not-selected";
 
-    if (this.state.selected) {
-      console.log(getCurrentColor());
-      
-
-      return <div 
-        className="px-palette-cell cell-selected" 
-        style={cellStyle} 
-        onClick={() => this.setState({ selected: ! this.state.selected })}></div>
-    }
-    else {
-        return <div 
-        className="px-palette-cell cell-not-selected" 
-        style={cellStyle} 
-        onClick={() => {
-          setCurrentColor(this.props.color);
-          this.setState({ selected: ! this.state.selected });
-        }}></div>
-    }
+    return <div className={cellClasses} style={cellStyle} onClick={this.clicked}></div>
   }
 }
 
+const paletteColors = [
+  "#000000",
+  "#333333",
+  "#666666",
+  "#999999",
+  "#BBBBBB",
+  "#FFFFFF",
+  "#aa33ee",
+  "#00FF00",
+  "#0000FF",
+  "#FF0000"
+];
 
 const domContainer = document.querySelector('#palette');
-ReactDOM.render(<Palette availableColors={["#00FF00", "#0000FF", "#FF0000"]}/>, domContainer);
+ReactDOM.render(<Palette availableColors={paletteColors} selectedColor="#00FF00"/>, domContainer);
 
 
 var canvas = document.getElementById('canvas');

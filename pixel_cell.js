199e0df3eabfc9cfc50efa00f963a438f0b0c013
artem <artem.the.freelancer@gmail.com>
2022-04-15 19:04:47 +0400

    Added Board component; added basic border logic

--- pixel_cell.js
+++ pixel_cell.js
@@ -33,7 +33,7 @@ class Palette extends React.Component {
         <PaletteCell color={color} selected={ this.state.selectedColor == color } actionWhenClicked={this.changeSelectedColor}/>
       </li>);
 
-    return <ul>{listCells}</ul>;
+    return <div id="palette"><ul>{listCells}</ul></div>;
   }
 }
 
@@ -55,6 +55,8 @@ class PaletteCell extends React.Component {
   }
 }
 
+var ctx; //global variable for canvas context
+
 const paletteColors = [
   "#000000",
   "#333333",
@@ -68,25 +70,157 @@ const paletteColors = [
   "#FF0000"
 ];
 
-const domContainer = document.querySelector('#palette');
-ReactDOM.render(<Palette availableColors={paletteColors} selectedColor="#00FF00"/>, domContainer);
 
-
-var canvas = document.getElementById('canvas');
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
     
+    this.state = { 
+      boardPxHeight: props.height * props.pixelHeight,
+      boardPxWidth: props.width * props.pixelWidth 
+    };
 
-if (canvas.getContext) {
-  var ctx = canvas.getContext('2d');
+    this.cellColors = this.initCellColors(this.props.height, this.props.width);
 
-  canvas.addEventListener("click", drawPixel);
+    this.handleClick = this.handleClick.bind(this);
+    this.setCellColor = this.setCellColor.bind(this);
+    this.drawCell = this.drawCell.bind(this);
+    this.drawBoardCells = this.drawBoardCells.bind(this);
+    this.setRandomBorders = this.setRandomBorders.bind(this);
+    this.drawBoardBorders = this.drawBoardBorders.bind(this);
+    this.isInsideBorder = this.isInsideBorder.bind(this);
+    
+  }
+
+  componentDidMount() {
+    var canvas = document.getElementById('canvas');
+        
+
+    if (canvas.getContext) {
+      ctx = canvas.getContext('2d');
+
+      canvas.addEventListener("click", this.handleClick);
+    }
+
+    this.setRandomBorders();
+    this.drawBoardCells();
+    this.drawBoardBorders();
+  }
+
+  handleClick(event) {
+    let cellX = Math.floor(event.offsetX / this.props.pixelWidth);
+    let cellY = Math.floor(event.offsetY / this.props.pixelHeight);
+
+    if ( this.isInsideBorder(cellX, cellY) ) {
+      this.setCellColor(cellX, cellY, getCurrentColor())
+      this.drawBoardCells();
+      this.drawBoardBorders();
+    }
+  }
+
+  initCellColors( height, width ) {
+    let cellColors = new Array(width);
+
+    for (let x = 0; x < width; x++) {
+      cellColors[x] = new Array(height);
+
+      for( let y = 0; y < height; y++) {
+        cellColors[x][y] = '#ffffff';
+      }
+    }
+    return cellColors;
+  }
+
+  setCellColor(x,y,color) {
+    this.cellColors[x][y] = color;
+  }
+
+  drawCell(x,y,color) {
+    let pixelX = x * this.props.pixelWidth;
+    let pixelY = y * this.props.pixelHeight;
+
+    if (!color) {
+      ctx.fillStyle = '#ffffff';
+    }
+    else {
+      ctx.fillStyle = color;
+    }
+
+    ctx.fillRect(pixelX, pixelY, this.props.pixelWidth, this.props.pixelHeight);
+  }
+
+  drawBoardCells() {
+    for (let x = 0; x < this.props.width; x++) {
+      for( let y = 0; y < this.props.height; y++) {
+        this.drawCell(x,y,this.cellColors[x][y])
+      }
+    }
+  }
+
+  setRandomBorders() {
+
+    let X1 = 0, Y1 = 0, X2 = 0, Y2 = 0;
+
+    let i = 0;
+    while ( X1 == Y2 || Y1 == Y2 ) { // make sure all coords are different
+      // define two sets of coordinates
+      X1 = Math.floor(Math.random() * this.props.width);
+      Y1 = Math.floor(Math.random() * this.props.height);
+      X2 = Math.floor(Math.random() * this.props.width);
+      Y2 = Math.floor(Math.random() * this.props.height);
+
+      console.log(X1,Y1,X2,Y2);
+
+      if (i++ > 100 ) break;
+    }
+    
+    // choose coords for top left corner
+    let Xs = X1 > X2 ? X2 : X1;
+    let Ys = Y1 > Y2 ? Y2 : Y1;
+
+    // choose coords for bottom right corner
+    let Xe = X1 > X2 ? X1 : X2;
+    let Ye = Y1 > Y2 ? Y1 : Y2;
+
+     
+    const topCorner = [Xs,Ys];
+    const bottomCorner = [Xe,Ye];
+      
+    this.state.borders = [topCorner, bottomCorner];
+    console.log('border', [topCorner, bottomCorner]);
+  }
+
+  isInsideBorder( cellX, cellY) {
+    const isInsideHorizontally = (this.state.borders[0][0] <= cellX) && (cellX < this.state.borders[1][0]);
+    const isInsideVertically = (this.state.borders[0][1] <= cellY) && (cellY < this.state.borders[1][1]);
+
+    return isInsideHorizontally && isInsideVertically;
+  }
+
+  drawBoardBorders() {
+    ctx.fillStyle = '#44eaed';
+
+    console.log(this.state);
+    
+    const borderTopX = this.state.borders[0][0] * this.props.pixelWidth;
+    const borderTopY = this.state.borders[0][1] * this.props.pixelHeight;
+
+    const borderWidth = this.state.borders[1][0] * this.props.pixelWidth - borderTopX;
+    const borderHeight = this.state.borders[1][1] * this.props.pixelHeight - borderTopY;
+
+    ctx.strokeStyle = '#44eaed';
+    ctx.strokeRect(borderTopX, borderTopY, borderWidth, borderHeight);
+  }
+
+  render() {
+    const boardStyle = { border: "1px solid black" };
+    return <canvas id="canvas" height={this.state.boardPxHeight} width={this.state.boardPxWidth} style={boardStyle} ></canvas>
+  }
 }
 
-var pixelWidth = 10;
-var pixelHeight = 10;
+const domContainer = document.querySelector('#board');
+ReactDOM.render(
+  <div><Board height="30" width="30" pixelHeight="10" pixelWidth="10" ></Board>
+  <Palette availableColors={paletteColors} selectedColor="#00FF00"/></div>, 
+  domContainer);
 
-function drawPixel(event) {
-  let pixelX = Math.floor(event.offsetX / pixelWidth) * pixelWidth;
-  let pixelY = Math.floor(event.offsetY / pixelHeight) * pixelHeight;
-  ctx.fillStyle = getCurrentColor();
-  ctx.fillRect(pixelX, pixelY, pixelWidth, pixelHeight);
-}
